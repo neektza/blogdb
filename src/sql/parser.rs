7d91fd3cdb10eb6ac87ea48cc6a5cd1fe1a7bffe
Nikica Jokic <neektza@gmail.com>
2016-06-23 12:57:43 +0200

    Remove SQL related stuff, we'll take care of that later

--- src/sql/parser.rs
+++
@@ -1,8 +0,0 @@
-#[macro_use]
-
-use nom::{is_alphanumeric};
-
-// named!(parens, delimited!(char!('('), is_not!(")"), char!(')')));
-// named!(alpha, take_while!( is_alphanumeric ) );
-
-named!(parens_num, delimited!(char!('('), take_while!(is_alphanumeric), char!(')')));
